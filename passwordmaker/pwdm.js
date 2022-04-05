514a1a494d970507a64b454b2f0d4fbe427b4b55
Change-37 <202110305@bssm.hs.kr>
2022-04-05 11:11:37 +0900

    2022-04-05

--- passwordmaker/pwdm.js
+++ passwordmaker/pwdm.js
@@ -8,52 +8,179 @@ const rec3 = document.getElementById('rec3');
 const rec4= document.getElementById('rec4');
 const recs = [rec1, rec2, rec3, rec4];
 
-function copy1(){
+function copy1()
+{
     rec1.select();
     document.execCommand('copy');
     console.log('copied');
 }
-function copy2(){
+function copy2()
+{
     rec2.select();
     document.execCommand('copy');
     console.log('copied');
 }
-function copy3(){
+function copy3()
+{
     rec3.select();
     document.execCommand('copy');
     console.log('copied');
 }
-function copy4(){
+function copy4()
+{
     rec4.select();
     document.execCommand('copy');
     console.log('copied');
 }
-function recommand(){
-    for(let i = 0; i < 4; i++){
+function recommand()
+{
+    for(let i = 0; i < 4; i++)
+    {
+        let Up = [];
+        let Dn = [];
+        let Un = [];
+        let Nu = [];
         recs[i].textContent = "";
         let pwd = [];
-        let n = Math.floor( Math.random() * 9 ) + 8;
-        for(let j = 0; j < n; j++){
-            let m = Math.floor( Math.random() * 4 )
-            switch (m){
+        let n = Math.floor( Math.random() * 5 ) + 8;
+        for(let j = 0; j < n; j++)
+        {
+            let m = Math.floor( Math.random() *10 )
+            switch (m%4)
+            {
                 case 0:
-                    recs[i].textContent += Math.floor( Math.random() * 10 );
+                    pwd[j] = Math.floor( Math.random() * 10 );
+                    Nu.push(j);
                     break;
                 case 1:
                     let a = Math.floor( Math.random() * alpha.length );
-                    recs[i].textContent += alpha[a];
+                    pwd[j] = alpha[a];
+                    Dn.push(j);
                     break;
                 case 2:
                     let b = Math.floor( Math.random() * BETA.length );
-                    recs[i].textContent += BETA[b];
+                    pwd[j] = BETA[b];
+                    Up.push(j);
                     break;
                 case 3:
                     let u = Math.floor( Math.random() * uni.length );
-                    recs[i].textContent += uni[u];
+                    pwd[j] = uni[u];
+                    Un.push(j);
                     break;
             }
         }
-
+        if(Nu.length == 0)
+        {
+            if(Un.length >= 2)
+            {
+                let id = Un[Un.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Nu.push(id);
+                Nu.sort(function(a, b) { return a - b; });
+                Un.pop();
+            }
+            else if(Up.length >= 2)
+            {
+                let id = Up[Up.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Nu.push(id);
+                Nu.sort(function(a, b) { return a - b; });
+                Up.pop();
+            }
+            else if(Dn.length >= 2)
+            {
+                let id = Dn[Dn.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Nu.push(id);
+                Nu.sort(function(a, b) { return a - b; });
+                Dn.pop();
+            }
+        }
+        if(Dn.length == 0)
+        {
+            if(Un.length >= 2)
+            {
+                let id = Un[Un.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Dn.push(id);
+                Dn.sort(function(a, b) { return a - b; });
+                Un.pop();
+            }
+            else if(Up.length >= 2)
+            {
+                let id = Up[Up.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Dn.push(id);
+                Dn.sort(function(a, b) { return a - b; });
+                Up.pop();
+            }
+            else if(Nu.length >= 2)
+            {
+                let id = Nu[Nu.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Dn.push(id);
+                Dn.sort(function(a, b) { return a - b; });
+                Nu.pop();
+            }
+        }
+        if(Up.length == 0)
+        {
+            if(Un.length >= 2)
+            {
+                let id = Un[Un.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Up.push(id);
+                Up.sort(function(a, b) { return a - b; });
+                Un.pop();
+            }
+            else if(Dn.length >= 2)
+            {
+                let id = Dn[Dn.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Up.push(id);
+                Up.sort(function(a, b) { return a - b; });
+                Dn.pop();
+            }
+            else if(Nu.length >= 2)
+            {
+                let id = Nu[Nu.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Up.push(id);
+                Up.sort(function(a, b) { return a - b; });
+                Nu.pop();
+            }
+        }
+        if(Un.length == 0)
+        {
+            if(Up.length >= 2)
+            {
+                let id = Up[Up.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Un.push(id);
+                Un.sort(function(a, b) { return a - b; });
+                Up.pop();
+            }
+            else if(Dn.length >= 2)
+            {
+                let id = Dn[Dn.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Un.push(id);
+                Un.sort(function(a, b) { return a - b; });
+                Dn.pop();
+            }
+            else if(Nu.length >= 2)
+            {
+                let id = Nu[Nu.length];
+                pwd[id] = Math.floor( Math.random() * 10 );
+                Un.push(id);
+                Un.sort(function(a, b) { return a - b; });
+                Nu.pop();
+            }
+        }
+        for(let j = 0; j < n; j++)
+        {
+            recs[i].textContent += pwd[j];
+        }
     }
     console.log("recommanded!");
 }
